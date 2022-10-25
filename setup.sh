c2cb7b6df410eee90c5b52edd05a6e3287b06305
David Gebler <davidgebler@gmail.com>
2022-10-25 20:05:21 +0100

    update setup

--- setup.sh
+++ setup.sh
@@ -1,1 +1,2 @@
-docker exec oauth-server bash -c 'chmod +x bin/console && bin/console doctrine:database:create && bin/console doctrine:schema:create && bin/console app:bootstrap && composer install && mkdir var/keys && openssl genrsa -out var/keys/private.key 2048 && openssl rsa -in var/keys/private.key -pubout -out var/keys/public.key && chmod -R 777 var/ && chmod -R 644 var/keys/*'
+#!/usr/bin/env bash
+docker exec oauth-server bash -c 'composer install && chmod +x bin/console && bin/console doctrine:database:create && bin/console doctrine:schema:create && bin/console app:bootstrap && mkdir var/keys && openssl genrsa -out var/keys/private.key 2048 && openssl rsa -in var/keys/private.key -pubout -out var/keys/public.key && chmod -R 777 var/ && chmod -R 644 var/keys/*'
